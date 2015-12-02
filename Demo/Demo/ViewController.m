eb65e142e65c5a2572a4874ab577d4343e05a403
maztaflex <iosdev1@DEV-TEAM1-MOBILEui-MacBook-Pro.local>
2015-12-02 10:56:52 +0900

    add source and sample video files

--- Demo/Demo/ViewController.m
+++ Demo/Demo/ViewController.m
@@ -8,20 +8,64 @@
 
 #import "ViewController.h"
 
+@import MediaPlayer;
+@import AVFoundation;
+
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIView *videoPlayContainer;
+
+@property (strong, nonatomic) MPMoviePlayerController *player;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+#pragma mark - View Cycle
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self configureLayout];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated
+{
+    [[UIApplication sharedApplication] setStatusBarHidden:YES];
+}
+
+#pragma mark - Private Method
+- (void)configureLayout
+{
+    // 비디오 플레이어 설정
+    self.player = [[MPMoviePlayerController alloc] initWithContentURL:[self getContentURLInBundleWithFileName:@"sampleVideo" ext:@"mp4"]];
+    self.player.scalingMode = MPMovieScalingModeAspectFill;
+    self.player.controlStyle = MPMovieControlStyleNone;
+    self.player.shouldAutoplay = YES;
+    self.player.repeatMode = MPMovieRepeatModeOne;
+    CGSize screenSize = [[UIScreen mainScreen] bounds].size;
+    [self.player.view setFrame:CGRectMake(0, 0, screenSize.width, screenSize.height)];
+    [self.player prepareToPlay];
+    
+    // 비디오 플레이어 서브뷰 등록
+    [self.videoPlayContainer addSubview:self.player.view];
+}
+
+// 번들 비디오 파일 URL
+- (NSURL *)getContentURLInBundleWithFileName:(NSString *)fileName ext:(NSString *)ext
+{
+    NSString *urlStr = [[NSBundle mainBundle] pathForResource:fileName ofType:ext];
+    NSURL *mUrl = [NSURL fileURLWithPath:urlStr];
+    
+    return mUrl;
+}
+
+// 원격 비디오 파일 URL
+- (NSURL *)getContentURLWithURLString:(NSString *)urlString
+{
+    NSURL *remoteURL = [NSURL URLWithString:urlString];
+    
+    return remoteURL;
 }
 
 @end
